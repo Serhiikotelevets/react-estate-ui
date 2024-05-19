4f2d4719a4009dc64a8dd2ad640c13b1587e17ae
Serhii <kotelevets1@gmai.com>
2024-05-19 22:36:31 +0300

    final commit

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Discover your perfect home with ease. <br/>
+          Our extensive listings and expert guidance make finding your dream place a breeze. <br/>
+          Start your journey today and turn your real estate dreams into reality.<br/>
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg4.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/routes/login/login.jsx
+++
@@ -1,7 +0,0 @@
-import './login.scss'
-
-export default function login() {
-    return(
-        <div className='Login'>login</div>
-    )
-}
